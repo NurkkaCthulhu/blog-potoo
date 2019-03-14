8d44351975fb97a4b401ede660a3d5734b23a3b3
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-14 13:40:52 +0200

    BlogPost.java: add constructors, getters and setters

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPost.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPost.java
@@ -1,7 +1,73 @@
 package fi.tuni.lesserpotoo.blogpotoo;
 
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 public class BlogPost {
+    int id;
+    String author;
+    String title;
+    String content;
+    LocalDateTime timeOfCreation;
+    LocalDateTime timeOfEdit;
+    int likes;
+
+    public BlogPost() {
+    }
+
+    public BlogPost(String author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.timeOfCreation = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimeOfCreation() {
+        return timeOfCreation;
+    }
+
+    public LocalDateTime getTimeOfEdit() {
+        return timeOfEdit;
+    }
+
+    public void setTimeOfEdit(LocalDateTime timeOfEdit) {
+        this.timeOfEdit = timeOfEdit;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
 }
