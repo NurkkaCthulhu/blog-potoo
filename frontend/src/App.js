dd04dfdb890bf7dfe06f6c66aea9b1cc98878422
Anu Malm <anu.m.malm@gmail.com>
2019-03-14 14:44:13 +0200

    App.js: add test code to see how React Router works.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,78 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
+function App() {
 
+    return (
+        <Router>
+            <div>
+                <Header />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/login" component={Login} />
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+
+class Login extends Component {
     state = {};
 
+    constructor() {
+        super();
+        this.hello = this.hello.bind(this);
+    }
+
     componentDidMount() {
-        setInterval(this.hello, 250);
+        this.hello();
+    }
+
+    componentWillUnmount() {
     }
 
     hello = () => {
-    fetch('/api/hello')
-.then(response => response.text())
-.then(message => {
-    this.setState({message: message});
-});
-};
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({message: message});
+            });
+    };
 
-render() {
-    return (
-        <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">{this.state.message}</h1>
-    </header>
-    <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    </div>
-);
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">{this.state.message}</h1>
+                </header>
+            </div>
+        );
+    }
 }
+
+function Header() {
+    return (
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </ul>
+    );
 }
 
 export default App;
