f8bebcfa9fc2e3e51882536c66bc771122e2a81e
Anu Malm <anu.m.malm@gmail.com>
2019-03-14 12:26:59 +0200

    Initial commit.

---
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogpotooApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.lesserpotoo.blogpotoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpotooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpotooApplication.class, args);
+	}
+
+}
