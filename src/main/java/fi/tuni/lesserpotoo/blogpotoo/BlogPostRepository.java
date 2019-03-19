4e3e5ea3f22f52e800c62d300dc42bac1ab7e68c
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-19 09:26:53 +0200

    BlogPostRepository: add find -methods for find by author and find by time of creation (asc and desc)

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPostRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPostRepository.java
@@ -2,5 +2,11 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BlogPostRepository extends CrudRepository<BlogPost, Integer> {
+    public List<BlogPost> findByAuthor(String author);
+    public List<BlogPost> findByTimeOfCreationOrderByTimeOfCreationAsc(LocalDateTime timeOfCreation);
+    public List<BlogPost> findByTimeOfCreationOrderByTimeOfCreationDesc(LocalDateTime timeOfCreation);
 }
