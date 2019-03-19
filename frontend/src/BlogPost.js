1fbe8bfc2b3dd87f6eae7d5a8b809d9321c435ae
Anu Malm <anu.m.malm@gmail.com>
2019-03-19 09:47:04 +0200

    BlogPost.js: Add basic h1 text for testing.

---
+++ frontend/src/BlogPost.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './frontpage_style.css';
+
+function BlogPost() {
+
+    return (
+        <h1>Blogipostaus</h1>
+    );
+}
+
+
+export default BlogPost;
