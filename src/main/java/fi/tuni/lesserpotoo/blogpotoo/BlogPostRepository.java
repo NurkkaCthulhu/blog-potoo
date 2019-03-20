fa0d293854b68ef776005957e41a987c2154e588
Anu Malm <anu.m.malm@gmail.com>
2019-03-20 13:24:56 +0200

    Revert "Revert "Merge branch 'dev' of https://github.com/NurkkaCthulhu/blog-potoo into dev""
    This reverts commit 92d652e02d49965cb33ff19b23d0e7943d8324fe.

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPostRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogPostRepository.java
@@ -2,11 +2,12 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, Integer> {
     public List<BlogPost> findByAuthor(String author);
-    public List<BlogPost> findByTimeOfCreationOrderByTimeOfCreationAsc(LocalDateTime timeOfCreation);
-    public List<BlogPost> findByTimeOfCreationOrderByTimeOfCreationDesc(LocalDateTime timeOfCreation);
+
+    // public List<BlogPost> findByTimeOfCreation_YearAndTimeOfCreation_MonthAndTimeOfCreation_DayOfMonth(int year, int month, int date);
+    // public List<BlogPost> findByTimeOfCreationOrderByTimeOfCreationDesc(LocalDateTime timeOfCreation);
 }
