93c638e506e61aab0b019cb811bccc7ae871b88d
Anu Malm <anu.m.malm@gmail.com>
2019-03-20 14:51:30 +0200

    NewPost.js: Make it possible to post a new example post.

--- frontend/src/NewPost.js
+++ frontend/src/NewPost.js
@@ -3,19 +3,39 @@ import './NewPost_style.css';
 
 class NewPost extends Component {
 
+    newPost = {
+        author: 'Potoo mom',
+        title: 'Days of Potooing',
+        content: 'It is good to be a potoo. I recommend. I feel despair but it is completely ok.',
+    };
+
     constructor() {
         super();
         this.makeNewPost = this.makeNewPost.bind(this);
+        this.state = {
+            item: this.newPost
+        };
     }
 
-    makeNewPost() {
+    async makeNewPost() {
+        const {item} = this.state;
 
+        await fetch('/api/blogposts/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        }).then(() => {
+            console.log('This should be posted.');
+        });
     }
 
     render() {
         return (
             <div className = "container">
-                <button className="newPost" onClick={this.makeNewPost}>New Post</button>
+                <button className="newPost" onClick={this.makeNewPost}>Add New Post</button>
             </div>
         );
     }
