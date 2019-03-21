71915393e0015178cf99e8560683ce19688951a8
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-21 17:46:58 +0200

    PostLoader.js: Instead of BlogPost id, send the whole BlogPost object to BlogPost.js-component. Delete NewPost-component call from return.

--- frontend/src/PostLoader.js
+++ frontend/src/PostLoader.js
@@ -23,7 +23,7 @@ class PostLoader extends Component {
         let helperArray = [];
 
         for (let obj of jsonObject) {
-            helperArray.push(<BlogPost key={obj.id} id={obj.id} sendData={this.updatePosts}/>);
+            helperArray.push(<BlogPost key={obj.id} id={obj} sendData={this.updatePosts}/>);
         }
 
         this.setState({arrayOfBlogPosts: helperArray});
@@ -36,7 +36,7 @@ class PostLoader extends Component {
         return (
             <div>
                 {this.state.arrayOfBlogPosts}
-                <NewPost sendData={this.updatePosts} />
+
             </div>
         );
     }
