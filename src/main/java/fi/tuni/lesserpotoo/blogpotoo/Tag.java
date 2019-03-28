7741807ab0b18dca03e99b6aff928a9bcbeefb05
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-28 14:18:27 +0200

    Tag: Add JsonIgnore to blogPosts

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/Tag.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/Tag.java
@@ -1,5 +1,7 @@
 package fi.tuni.lesserpotoo.blogpotoo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -13,6 +15,7 @@ public class Tag {
     String tagName;
 
     @ManyToMany(mappedBy = "tags", fetch = FetchType.EAGER)
+    @JsonIgnore
     Set<BlogPost> blogPosts;
 
     public Tag() {}
