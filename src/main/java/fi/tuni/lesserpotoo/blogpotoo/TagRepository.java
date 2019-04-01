7a4dc51bcce351f60528a63c666a64e90c4816dd
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-01 12:31:17 +0300

    TagRepository: add method for finding tags by TagName, ignoring case

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/TagRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/TagRepository.java
@@ -2,5 +2,8 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends CrudRepository<Tag, Integer> {
+    Optional<Tag> findTagByTagNameIgnoreCase(String tagName);
 }
