8ac763f734a4f8dc61c64ae47864daf1e5377c0e
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-03 17:36:37 +0300

    SearchBar.js: Links to search page when search button is pushed

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,15 +1,23 @@
 import React, {Component} from "react";
 import './css/search_style.css';
+import { Link } from "react-router-dom";
 
 class SearchBar extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {searchWord: ''}
+    }
+
+    handleChange(event) {
+        let value = event.target.value;
+        this.setState({searchWord: value});
     }
 
     render() {
         return (<div className="search">
-            <input type="text" placeholder="Search titles, tags or authors..." name="searchBar" />
-            <button type="submit"><i className='fas fa-search'></i></button>
+            <input type="text" placeholder="Search titles, tags or authors..." name="searchBar" onChange={this.handleChange} />
+            <Link to={'/search/' + this.state.searchWord}><button type="submit"><i className='fas fa-search'></i></button></Link>
         </div>);
     }
 }
