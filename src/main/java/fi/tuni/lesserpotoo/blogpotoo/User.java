c8404babb3d1b0a14eb034bf384b9afb9014d4d0
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-17 11:43:42 +0300

    User: make attributes private, add toString

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
@@ -7,16 +7,16 @@ public class User {
     @Id
     @GeneratedValue(generator = "user_seq")
     @SequenceGenerator(name = "user_seq", sequenceName = "USER_SEQ", allocationSize = 1)
-    int id;
+    private int id;
 
     @Column(nullable = false)
-    String userName;
+    private String userName;
 
     @Column(nullable = false)
-    String password;
+    private String password;
 
     @Column(nullable = false)
-    UserType userType;
+    private UserType userType;
 
     public User() {
     }
@@ -53,5 +53,15 @@ public class User {
 
     public void setUserType(UserType userType) {
         this.userType = userType;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", userType=" + userType +
+                '}';
     }
 }
