39785517f06fa4ce9d44ba55f00d9b127734791e
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-17 11:37:30 +0300

    UserType: add getters and setters for attributes

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
@@ -26,4 +26,32 @@ public class User {
         this.password = password;
         this.userType = userType;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
