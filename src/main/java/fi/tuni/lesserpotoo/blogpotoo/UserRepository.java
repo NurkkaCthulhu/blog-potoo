ce6223d2870cb663deed25053105968c983c6364
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-17 15:22:33 +0300

    UserRepository: add method to find by username and password

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/UserRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/UserRepository.java
@@ -2,5 +2,8 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByUserNameAndPasswordIn(String userName, String password);
 }
