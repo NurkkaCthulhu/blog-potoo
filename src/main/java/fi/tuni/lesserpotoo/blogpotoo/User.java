e699080eae713785dedecdef40fa577bff00274b
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-17 11:36:06 +0300

    UserType: add attributes id, userName, password and userType

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/User.java
@@ -1,4 +1,20 @@
 package fi.tuni.lesserpotoo.blogpotoo;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "USER_SEQ", allocationSize = 1)
+    int id;
+
+    @Column(nullable = false)
+    String userName;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    UserType userType;
 }
