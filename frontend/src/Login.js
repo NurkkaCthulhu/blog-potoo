dbc9ce06d78fa2d30edd92c8b3170a2ffbddb66c
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-17 15:03:41 +0300

    Login.js: add placeholder functions for login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,14 +2,41 @@ import React, {Component} from "react";
 import './css/Login_style.css';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.logUserIn = this.logUserIn.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        let loggedin = false;
+
+        if (localStorage.getItem('loggedin') === true) {
+            loggedin = true;
+        }
+
+        this.state = {loggedin: loggedin
+            , errorMessage: ''
+            , username: ''
+            , password: ''}
+    }
+
+    handleChange(event) {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({[name]: value})
+    }
+
+    logUserIn() {
+        this.setState({errorMessage: <div className="loginErrorMessage">Username or password is incorrect.</div>});
+        console.log(this.state);
+    }
+
     render() {
         return (<div className="loginContainer">
             <h1>Login</h1>
-            <form>
-                <input type="text" className="textInput" placeholder="Username" />
-                <br/><input type="text" className="textInput" placeholder="Password"  />
-                <br/><button type="submit" className="buttonInput">Login</button>
-            </form>
+            <input name="username" type="text" className="textInput" placeholder="Username" onChange={this.handleChange} />
+            <br/><input name="password" type="password" className="textInput" placeholder="Password" onChange={this.handleChange}  />
+            <br/><button type="submit" onClick={this.logUserIn} className="buttonInput">Login</button>
+
+            {this.state.errorMessage}
 
             <p>No user? Register <a href="/register">here</a>!</p>
         </div>);
