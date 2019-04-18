8f07b483cd63f37fec6e7b08d64d46af388de568
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-18 17:41:31 +0300

    UserRepository: add findUserByUsernameIgnoreCase()

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/UserRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/UserRepository.java
@@ -2,8 +2,10 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsernameIgnoreCaseAndPasswordIn(String username, String password);
+    Optional<User> findByUsernameIgnoreCase(String username);
 }
