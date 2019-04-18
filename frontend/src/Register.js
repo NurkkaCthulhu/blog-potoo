905f4a3725ca7295dc6a714da23cb5fffbe1f096
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-18 17:18:23 +0300

    Register.js: checks if given passwords are the same

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -4,17 +4,21 @@ import './css/Login_style.css';
 class Register extends Component {
     constructor() {
         super();
-        this.state = {errorMessage: ''}
+        this.state = {errorMessage: '', username: '', password: '', passwordAgain: ''}
         this.handleChange = this.handleChange.bind(this);
         this.registerUser = this.registerUser.bind(this);
     }
 
     handleChange(event) {
-
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({[name]: value})
     }
 
     registerUser(event) {
-
+        if (this.state.password !== this.state.passwordAgain) {
+            this.setState({errorMessage: <div className="loginErrorMessage">Given passwords do not match.</div>});
+        }
     }
 
     render() {
@@ -22,7 +26,7 @@ class Register extends Component {
             <h1 className="loginH1">Register</h1>
             <input name="username" type="text" className="textInput" placeholder="Username" onChange={this.handleChange} />
             <br/><input name="password" type="password" className="textInput" placeholder="Password" onChange={this.handleChange}  />
-            <br/><input name="password" type="password" className="textInput" placeholder="Password again" onChange={this.handleChange}  />
+            <br/><input name="passwordAgain" type="password" className="textInput" placeholder="Password again" onChange={this.handleChange}  />
             <br/><button type="submit" onClick={this.registerUser} className="buttonInput">Register</button>
 
             {this.state.errorMessage}
