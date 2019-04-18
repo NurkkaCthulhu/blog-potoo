9edcb54d41c4919eb9467e2c8287f626f07e6d7a
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-18 16:51:02 +0300

    Header.js: show different links if user is logged in. Add logout feature

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,22 +1,46 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './css/frontpage_style.css';
-import PostLoader from './PostLoader';
-import NewPostForm from './NewPostForm';
-import BlogPost from './BlogPost';
-import ErrorPage from "./ErrorPage";
-import Login from "./Login";
-import Register from "./Register";
 
 class Header extends Component {
+    constructor() {
+        super();
+        let loggedIn = false;
+        if (localStorage.getItem('loggedin') === 'true') {
+            loggedIn = true;
+        }
+        this.state = {loggedIn: loggedIn};
+
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        localStorage.removeItem('userId');
+        localStorage.removeItem('loggedin');
+        localStorage.removeItem('userType');
+        this.setState({loggedIn: false});
+    }
+
     render() {
+        if (!this.state.loggedIn && localStorage.getItem('loggedin') === 'true') {
+            this.setState({loggedIn: true})
+        }
+
         return (
-            <nav className="header">
-                <Link to="/"><i className='fab fa-earlybirds'></i>Blog Potoo</Link>
-                <Link to="/register" className="registerlink">Register</Link>
-                <Link to="/login" className="loginlink">LogIn</Link>
-                <Link to="/blogposts/modifypost/new" className="newpostlink">Add blog post...</Link>
-            </nav>
+            <div>
+                {this.state.loggedIn ?
+                <nav className="header">
+                    <Link to="/"><i className='fab fa-earlybirds'></i>Blog Potoo</Link>
+                    <a onClick={this.logout} href="" className="registerlink">Logout</a>
+                    <Link to="/blogposts/modifypost/new" className="newpostlink">Add blog post...</Link>
+                </nav>
+                :
+                 <nav className="header">
+                    <Link to="/"><i className='fab fa-earlybirds'></i>Blog Potoo</Link>
+                    <Link to="/register" className="registerlink">Register</Link>
+                    <Link to="/login" className="loginlink">LogIn</Link>
+                </nav>}
+            </div>
         );
     }
 }
