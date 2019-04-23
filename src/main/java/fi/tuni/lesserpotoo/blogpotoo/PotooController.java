0a07495eb4c9d3c56598d7e66e33c6ae54606897
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 14:40:59 +0300

    PotooController: add DeleteMapping for comments

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/PotooController.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/PotooController.java
@@ -169,6 +169,19 @@ public class PotooController {
         }
     }
 
+    @DeleteMapping("/api/blogposts/{blogPostId}/comments/{commentId}")
+    public void deleteCommentFromABlogPost(@PathVariable int blogPostId, @PathVariable int commentId) {
+        Optional<BlogPost> blogPostOptional = blogPostRepository.findById(blogPostId);
+        Optional<Comment> commentOptional = commentRepository.findById(commentId);
+
+        if (blogPostOptional.isPresent() && commentOptional.isPresent()) {
+            if (blogPostOptional.get().getComments().contains(commentOptional.get())) {
+                blogPostOptional.get().getComments().remove(commentOptional.get());
+                commentRepository.delete(commentOptional.get());
+            }
+        }
+    }
+
     @DeleteMapping("/api/users/{userId}")
     public void makeUserTypeDeletedById(@PathVariable int userId) {
         Optional<User> userOptional = userRepository.findById(userId);
