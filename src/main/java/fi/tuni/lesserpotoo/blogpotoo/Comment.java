1a590ffd4a2c0084193bde0e5c22f172afe345bd
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 13:25:32 +0300

    Comment: add attributes

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/Comment.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/Comment.java
@@ -1,7 +1,20 @@
 package fi.tuni.lesserpotoo.blogpotoo;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 public class Comment {
+    @Id
+    @GeneratedValue(generator = "comment_seq")
+    @SequenceGenerator(name = "comment_seq", sequenceName = "COMMENT_SEQ", allocationSize = 1)
+    int id;
+
+    User commentor;
+    BlogPost blogPost;
+    LocalDate dateOfComment;
+    LocalTime timeOfComment;
+
+    String content;
 }
