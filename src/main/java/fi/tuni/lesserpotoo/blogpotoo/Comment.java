d4755f0ebfcf984092431755a120b760c2a2d463
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 13:29:43 +0300

    Comment: add annotations to attributes

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/Comment.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/Comment.java
@@ -11,10 +11,20 @@ public class Comment {
     @SequenceGenerator(name = "comment_seq", sequenceName = "COMMENT_SEQ", allocationSize = 1)
     int id;
 
+    @ManyToOne
+    @JoinColumn
     User commentor;
+
+    @ManyToOne
+    @JoinColumn
     BlogPost blogPost;
+
+    @Column(nullable = false)
     LocalDate dateOfComment;
+
+    @Column(nullable = false)
     LocalTime timeOfComment;
 
+    @Column(nullable = false)
     String content;
 }
