8e1990c19a66763c77ce8aaf2eefcb9bb7df2839
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 12:29:14 +0300

    App, Header: make changes between mobile and desktop automatic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './css/frontpage_style.css';
 import PostLoader from './PostLoader';
@@ -7,7 +7,6 @@ import ErrorPage from "./ErrorPage";
 import Login from "./Login";
 import Register from "./Register";
 import Header from "./Header";
-
 
 function Index() {
     return <PostLoader />;
@@ -25,28 +24,46 @@ function RegisterUser() {
     return <Register />
 }
 
-function App() {
-    let dynamicClassName = window.innerWidth >= 500 ? "desktop" : "mobile";
+class App extends Component {
+    constructor() {
+        super();
 
-    return (
-        <Router>
-            <div>
-                <Header />
-                <div className={"blog-posts " + dynamicClassName}>
-                    <Switch>
-                        <Route exact path="/" exact component={Index} />
-                        <Route exact path="/search/:search" component={PostLoader} />
-                        <Route exact path="/blogposts/modifypost/:id" component={NewPostForm} />
-                        <Route exact path={"/blogposts/:id"} component={PostLoader}/>
-                        <Route exact path="/login" component={LoginUser}/>
-                        <Route exact path="/register" component={RegisterUser}/>
-                        <Route component={Error}/>
-                    </Switch>
+        let monitorType = window.innerWidth >= 500 ? 'desktop' : 'mobile';
+        this.state = {monitorType: monitorType}
+
+        window.onresize = this.checkWindowSize.bind(this);
+    }
+
+    checkWindowSize() {
+        if (window.innerWidth >= 500) {
+            this.setState({monitorType: 'desktop'});
+        } else {
+            this.setState({monitorType: 'mobile'});
+        }
+    }
+
+    render() {
+
+        return (
+            <Router>
+                <div>
+                    <Header monitorType={this.state.monitorType} />
+                    <div className={"blog-posts " + this.state.monitorType}>
+                        <Switch>
+                            <Route exact path="/" exact component={Index} />
+                            <Route exact path="/search/:search" component={PostLoader} />
+                            <Route exact path="/blogposts/modifypost/:id" component={NewPostForm} />
+                            <Route exact path={"/blogposts/:id"} component={PostLoader}/>
+                            <Route exact path="/login" component={LoginUser}/>
+                            <Route exact path="/register" component={RegisterUser}/>
+                            <Route component={Error}/>
+                        </Switch>
+                    </div>
+                    <div className="footer"><span>Blog Potoo, the blog of the future (2019)</span></div>
                 </div>
-                <div className="footer"><span>Blog Potoo, the blog of the future (2019)</span></div>
-            </div>
-        </Router>
-    );
+            </Router>
+        );
+    }
 }
 
 
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -3,12 +3,13 @@ import { Link} from "react-router-dom";
 import './css/frontpage_style.css';
 
 class Header extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         let loggedIn = false;
         if (localStorage.getItem('loggedin') === 'true') {
             loggedIn = true;
         }
+
         this.state = {loggedIn: loggedIn};
 
         this.logout = this.logout.bind(this);
@@ -26,21 +27,21 @@ class Header extends Component {
         if (!this.state.loggedIn && localStorage.getItem('loggedin') === 'true') {
             this.setState({loggedIn: true})
         }
-        let windowWidth = window.innerWidth;
+
         let welcomeMessage = 'Welcome, ' + localStorage.getItem('username') + '!';
 
         return (
             <div>
                 {this.state.loggedIn ?
                 <nav className="header">
-                    <Link to="/" className="frontpagelink"><i className='fab fa-earlybirds'></i>{windowWidth >= 500 && "Blog Potoo"}</Link>
-                    <Link to="/" onClick={this.logout} className="registerlink">{windowWidth >= 500 ? "Logout" : <i className='fas fa-sign-out-alt'></i>}</Link>
-                    <Link to="/blogposts/modifypost/new" className="newpostlink">{windowWidth >= 500 ? "New post" : <i className='fas fa-edit'></i>}</Link>
-                    <Link to="/" className="userpagelink">{windowWidth >= 500 ? welcomeMessage : <i className='fas fa-user-alt'></i>}</Link>
+                    <Link to="/" className="frontpagelink"><i className='fab fa-earlybirds'></i>{this.props.monitorType === 'desktop' && "Blog Potoo"}</Link>
+                    <Link to="/" onClick={this.logout} className="registerlink">{this.props.monitorType === 'desktop' ? "Logout" : <i className='fas fa-sign-out-alt'></i>}</Link>
+                    <Link to="/blogposts/modifypost/new" className="newpostlink">{this.props.monitorType === 'desktop' ? "New post" : <i className='fas fa-edit'></i>}</Link>
+                    <Link to="/" className="userpagelink">{this.props.monitorType === 'desktop' ? welcomeMessage : <i className='fas fa-user-alt'></i>}</Link>
                 </nav>
                 :
                  <nav className="header">
-                    <Link to="/"><i className='fab fa-earlybirds'></i>{windowWidth >= 500 && "Blog Potoo"}</Link>
+                    <Link to="/"><i className='fab fa-earlybirds'></i>{this.props.monitorType === 'desktop' && "Blog Potoo"}</Link>
                     <Link to="/register" className="registerlink">Register</Link>
                     <Link to="/login" className="loginlink">LogIn</Link>
                 </nav>}
