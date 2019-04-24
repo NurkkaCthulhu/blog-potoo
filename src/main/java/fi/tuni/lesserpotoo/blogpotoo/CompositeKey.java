15e3f15853b497722a61312bbd299e702d41b9c0
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 09:37:39 +0300

    CompositeKey: create file with initial code

---
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/CompositeKey.java
@@ -0,0 +1,8 @@
+package fi.tuni.lesserpotoo.blogpotoo;
+
+import java.io.Serializable;
+
+public class CompositeKey implements Serializable {
+    private int userId;
+    private int blogPostId;
+}
