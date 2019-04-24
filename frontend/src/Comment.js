db58ba1bf8e2d18d4f7c92af36fa46eaf3459416
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-24 15:58:08 +0300

    Comment.js: Add delete button for admin.

--- frontend/src/Comment.js
+++ frontend/src/Comment.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import './css/Comment_style.css';
+import {Link} from "react-router-dom";
 
 class Comment extends Component {
     constructor(props) {
@@ -7,10 +8,16 @@ class Comment extends Component {
         //console.log('props in comment ', props);
     }
 
+
     render() {
         return (
             <div className="comment">
                 <div className="commentContent">
+                    {localStorage.getItem('userType') === 'ADMIN' &&
+                    <Link to="/">
+                        <button className="deletebutton" onClick={this.deleteComment}><i className='fas fa-trash-alt'></i></button>
+                    </Link>
+                    }
                     <h3>{this.props.comment.commenter.username}</h3>
                     <small>{this.props.comment.dateOfComment + " at " + this.props.comment.timeOfComment}</small>
                     <p>{this.props.comment.content}</p>
