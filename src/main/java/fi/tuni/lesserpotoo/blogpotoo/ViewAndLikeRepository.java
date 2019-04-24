39677b4e682125cb0a3c3f901015eaa95d2e1e34
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 10:13:43 +0300

    ViewAndLikeRepository: add findByUserIdAndBlogPostId

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/ViewAndLikeRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/ViewAndLikeRepository.java
@@ -2,5 +2,9 @@ package fi.tuni.lesserpotoo.blogpotoo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.swing.text.View;
+import java.util.Optional;
+
 public interface ViewAndLikeRepository extends CrudRepository<ViewAndLike, CompositeKey> {
+    Optional<ViewAndLike> findByUserIdAndBlogPostId(int userId, int blogPostId);
 }
