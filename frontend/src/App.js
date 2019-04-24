6b4224356c533a7adad5886c21ae8f71e6af7f6c
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-24 10:18:20 +0300

    App.js: Remove unused imports and Archive-div.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './css/frontpage_style.css';
 import PostLoader from './PostLoader';
 import NewPostForm from './NewPostForm';
-import BlogPost from './BlogPost';
 import ErrorPage from "./ErrorPage";
 import Login from "./Login";
 import Register from "./Register";
@@ -32,9 +31,6 @@ function App() {
         <Router>
             <div>
                 <Header />
-                <div className="right-float">
-                    <p>Archives</p>
-                </div>
                 <div className="blog-posts">
                     <Switch>
                         <Route exact path="/" exact component={Index} />
