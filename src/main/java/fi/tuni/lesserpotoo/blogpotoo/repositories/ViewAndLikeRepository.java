3fc88a19c2c572b60ffe16973defbd63bc3c4984
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 19:22:38 +0300

    ViewAndLikeRepository: add methods to fin only by userId or blogPostId

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/ViewAndLikeRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/ViewAndLikeRepository.java
@@ -4,6 +4,7 @@ import fi.tuni.lesserpotoo.blogpotoo.misc.CompositeKey;
 import fi.tuni.lesserpotoo.blogpotoo.entities.ViewAndLike;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,4 +14,6 @@ import java.util.Optional;
  */
 public interface ViewAndLikeRepository extends CrudRepository<ViewAndLike, CompositeKey> {
     Optional<ViewAndLike> findByUserIdAndBlogPostId(int userId, int blogPostId);
+    List<ViewAndLike> findAllByUserId(int userId);
+    List<ViewAndLike> findAllByBlogPostId(int blogPostId);
 }
