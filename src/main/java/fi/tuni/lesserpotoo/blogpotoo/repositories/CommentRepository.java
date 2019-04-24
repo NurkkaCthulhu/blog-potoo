80f57f56306ab73952e33d79efdb6cd7017050d8
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 18:39:10 +0300

    CommentRepository: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/CommentRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/CommentRepository.java
@@ -3,5 +3,10 @@ package fi.tuni.lesserpotoo.blogpotoo.repositories;
 import fi.tuni.lesserpotoo.blogpotoo.entities.Comment;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-0424
+ * @since 3.0
+ */
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
 }
