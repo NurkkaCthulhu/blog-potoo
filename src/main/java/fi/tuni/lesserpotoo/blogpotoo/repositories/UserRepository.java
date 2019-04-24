798ecd07d3f43a35511b1c03d0dfd455feb6d8b0
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-24 18:40:49 +0300

    UserRepository: Add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/UserRepository.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/repositories/UserRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-0418
+ * @since 3.0
+ */
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsernameIgnoreCaseAndPasswordIn(String username, String password);
     Optional<User> findByUsernameIgnoreCase(String username);
