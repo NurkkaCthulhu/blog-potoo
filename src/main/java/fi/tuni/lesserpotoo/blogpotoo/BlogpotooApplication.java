5971780db1a59abc18c0e48c93a5d32c5057adcd
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-24 20:53:11 +0300

    BlogpotooApplication: add some curls (made by Essi)

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogpotooApplication.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/BlogpotooApplication.java
@@ -17,6 +17,18 @@ public class BlogpotooApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("cURL command for testing:\n"
+			+ "POST User: curl -d \"{\"username\":\"testUser\",\"password\":\"password\"}\",\"userType\":1}\" -H \"Content-Type:application/json\" -X -POST http://localhost:8080/api/users\n"
+			+ "POST BlogPost: curl -d \"{\"userId\":1,\"title\":\"test title\"}\",\"content\":\"test content\"}\" -H \"Content-Type:application/json\" -X -POST http://localhost:8080/api/blogposts\n"
+			+ "POST Tags: curl -d \"[\"new tag1\", \"new tag2\"]\" -H \"Content-Type:application/json\" -X -POST http://localhost:8080/api/blogposts/1/tag\n"
+			+ "POST Comment: curl -d \"{\"userId\":1,\"content\":\"test comment\"}\"}\" -H \"Content-Type:application/json\" -X -POST http://localhost:8080/api/blogposts/1/comment\n"
+			+ "\n"
+			+ "DELETE BlogPosts: curl -X DELETE http://localhost:8080/api/blogposts/2 \n"
+			+ "DELETE Users: curl -X GET http://localhost:8080/api/users/3 \n"
+			+ "\n"
+			+ "GET BlogPosts: curl -X GET http://localhost:8080/api/blogposts \n"
+			+ "GET Users: curl -X GET http://localhost:8080/api/users \n"
+		);
 		SpringApplication.run(BlogpotooApplication.class, args);
 	}
 
