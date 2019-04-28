d4d6cf28403f80f88ff33e98ed93a3ce4dc284bd
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 22:15:30 +0300

    NoSuchCommentUnderThisBlogPostException: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoSuchCommentUnderThisBlogPostException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoSuchCommentUnderThisBlogPostException.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
- * @since 3.1
+ * @since 3.0
  */
 @ResponseStatus(value = HttpStatus.NO_CONTENT)
 public class NoSuchCommentUnderThisBlogPostException extends RuntimeException {
+
+    /**
+     * Creates instance of NoSuchCommentUnderThisBlogPostException with message.
+     *
+     * Message is in following form: "BlogPost with id [blogPostId] does not have comment with id [commentId]"
+     *
+     * @param blogPostId    id of BlogPost
+     * @param commentId     id of Comment
+     */
     public NoSuchCommentUnderThisBlogPostException(int blogPostId, int commentId) {
         super("BlogPost with id " + blogPostId + " does not have comment with id " + commentId);
     }
