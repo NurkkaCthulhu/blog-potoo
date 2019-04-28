8d439d635b0d44b929676ade963729adfddd2202
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 22:16:41 +0300

    UsernameAlreadyUsedException: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/UsernameAlreadyUsedException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/UsernameAlreadyUsedException.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
- * @since 3.1
+ * @since 3.0
  */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UsernameAlreadyUsedException extends RuntimeException {
+
+    /**
+     * Creates instance of UsernameAlreadyUsedException with message.
+     *
+     * Message is in following form: "Username "[username]" already in use."
+     *
+     * @param username
+     */
     public UsernameAlreadyUsedException(String username) {
         super("Username \"" + username + "\" already in use.");
     }
