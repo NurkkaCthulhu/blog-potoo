0d6378ef68f01e9cc8aa09d797ec6d542f5fa75c
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 22:06:54 +0300

    BlogPostNotFoundException: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/BlogPostNotFoundException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/BlogPostNotFoundException.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
- * @since 3.1
+ * @since 3.0
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BlogPostNotFoundException extends RuntimeException {
+
+    /**
+     * Creates instance of BlogPostNotFoundException with message.
+     *
+     * @param id    BlogPost id
+     */
     public BlogPostNotFoundException(int id) {
         super("BlogPost with id " + id + " not found.");
     }
