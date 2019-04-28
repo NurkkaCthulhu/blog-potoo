5371d1a52941bf4439513f3158f1e471595ae567
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 17:08:29 +0300

    NoNeededValuesInBodyException: add ResponseStatus annotation

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoNeededValuesInBodyException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoNeededValuesInBodyException.java
@@ -1,10 +1,14 @@
 package fi.tuni.lesserpotoo.blogpotoo.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
  * @since 3.1
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NoNeededValuesInBodyException extends RuntimeException {
 
     public NoNeededValuesInBodyException(String... valueNames) {
