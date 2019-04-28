fb51e757c977f6c716200b5ba684d5ea21fddcef
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 22:18:08 +0300

    UserNotFoundException: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/UserNotFoundException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/UserNotFoundException.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
- * @since 3.1
+ * @since 3.0
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
+
+    /**
+     * Creates instance of UserNotFoundException with message.
+     *
+     * Message is in following form: "User with id [id] not found."
+     *
+     * @param id    id of User
+     */
     public UserNotFoundException(int id) {
         super("User with id " + id + " not found.");
     }
