cf27c8bb0ad6d0fddfde44d5e0f5d8cf7bdde258
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 21:56:22 +0300

    LoginFailedException: Add ResponseStatus annotation

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/LoginFailedException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/LoginFailedException.java
@@ -1,10 +1,14 @@
 package fi.tuni.lesserpotoo.blogpotoo.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
  * @since 3.0
  */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class LoginFailedException extends RuntimeException {
     public LoginFailedException() {
         super("Username or password is incorrect.");
