fd74fd7971d9b727b297e1bbd3b81fe939b97dde
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-28 22:12:27 +0300

    NoNeededValuesInBodyException: add javadoc

--- src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoNeededValuesInBodyException.java
+++ src/main/java/fi/tuni/lesserpotoo/blogpotoo/exceptions/NoNeededValuesInBodyException.java
@@ -6,22 +6,36 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * @author Essi Supponen [essi.supponen@tuni.fi]
  * @version 2019-0428
- * @since 3.1
+ * @since 3.0
  */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NoNeededValuesInBodyException extends RuntimeException {
 
+    /**
+     * Creates instance of NoNeededValuesInBodyException with given error message.
+     *
+     * @param message
+     */
     public NoNeededValuesInBodyException(String message) {
         super(message);
     }
 
-    public static NoNeededValuesInBodyException parseException(String... valueNames) {
-        String message = "RequestBody needs to have following values: ";
+    /**
+     * Parses a message from given value names and returns NoNeededValuesInBodyException object with that message.
+     *
+     * Goes trough list of strings and adds them to string, that is used as message. Message is in following form:
+     * "RequestBody needs to have following keys with values: [list of values]"
+     *
+     * @param keys   keys to values, that were not found
+     * @return NoNeededValuesInBodyException with parsed message
+     */
+    public static NoNeededValuesInBodyException parseException(String... keys) {
+        String message = "RequestBody needs to have following keys with values: ";
 
-        for (int i = 0; i < valueNames.length; i++) {
-            message += valueNames[i];
+        for (int i = 0; i < keys.length; i++) {
+            message += keys[i];
 
-            if (i < valueNames.length -1) {
+            if (i < keys.length -1) {
                 message += ", ";
             }
         }
